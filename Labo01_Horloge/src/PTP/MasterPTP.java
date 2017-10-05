f3f70b683c3fd8e1e98b3911fbdf681ff7c0c199
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2017-10-05 11:35:22 +0200

    minor change

--- Labo01_Horloge/src/PTP/MasterPTP.java
+++ Labo01_Horloge/src/PTP/MasterPTP.java
@@ -21,42 +21,40 @@ import java.util.logging.Logger;
 public class MasterPTP {
 
     private static final int BROADCAST_PORT = 1234;
-    private static final String GROUP_ADDRESS = "228.5.6.7";
+    private static final String GROUP_ADDRESS = "234.56.78.9";
     private static final long SYNC_PERIOD = 4000;
 
+    MulticastSocket broadcastSocket = new MulticastSocket(BROADCAST_PORT);
+    InetAddress group = InetAddress.getByName(GROUP_ADDRESS);
+
     private TimerTask syncTask = new TimerTask() {
-	MulticastSocket broadcastSocket = new MulticastSocket(BROADCAST_PORT);
-	InetAddress group = InetAddress.getByName(GROUP_ADDRESS);
 
 	private byte id = 0;
-	
+
 	@Override
 	public void run() {
 	    try {
 		// SYNC
 		System.out.println("Sending sync");
-		byte[] syncData = {0,  id};
-		
+		byte[] syncData = {0, id};
+
 		DatagramPacket packet = new DatagramPacket(syncData, syncData.length, group, BROADCAST_PORT);
 		broadcastSocket.send(packet);
-		
+
 		System.out.println("Sync sent");
-		
-		
-		
+
 		// ----------------
 		// FOLLOW_UP  {1, time, id}
-		
 		System.out.println("Sending follow_up");
 		long time = System.currentTimeMillis();
-		byte[] followUpData = ByteBuffer.allocate(2 + Long.BYTES).put(id).putLong(time).put(id).array();		
+		byte[] followUpData = ByteBuffer.allocate(2 + Long.BYTES).put(id).putLong(time).put(id).array();
 		packet = new DatagramPacket(followUpData, followUpData.length, group, BROADCAST_PORT);
 		broadcastSocket.send(packet);
-		System.out.println("Follow_up sent ("+id+")");
-		
+		System.out.println("Follow_up sent (" + id + ")");
+
 		//Increasing id
 		id++;
-		
+
 	    } catch (IOException ex) {
 		Logger.getLogger(MasterPTP.class.getName()).log(Level.SEVERE, null, ex);
 	    }
@@ -64,11 +62,17 @@ public class MasterPTP {
     };
     private final Timer syncTimer = new Timer();
 
+    //Delay request
+    private final Thread delayRequestThread = new Thread(() -> {
+	//broadcastSocket.receive();
+    });
+
     public MasterPTP() throws SocketException, IOException {
 	//Diffusion des messages sync et followup
 	syncTimer.scheduleAtFixedRate(syncTask, 0, SYNC_PERIOD);
-	
+
 	//Attente des delay_request
+	delayRequestThread.start();
     }
 
 }
