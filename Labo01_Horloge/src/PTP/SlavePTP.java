90f542bf5faef6414061549fbd871b627167829c
MiguelPomboDias <miguel.pombodias@heig-vd.ch>
2017-10-26 10:55:04 +0200

    update main project with to be deleted one

--- Labo01_Horloge/src/PTP/SlavePTP.java
+++ Labo01_Horloge/src/PTP/SlavePTP.java
@@ -22,66 +22,71 @@ public class SlavePTP {
    private static final int TIMEOUT = 4000;
 
    private static final int BUFFER_SIZE = 2;
+   
+   private int delay;
 
    private InetAddress masterAddress;
+   private MulticastSocket socket;
 
+   public SlavePTP() throws SocketException, IOException {
+      //Diffusion des messages sync et followup
+      startThread();
+      //syncTimer.scheduleAtFixedRate(syncTask, 0, SYNC_PERIOD);
+   }
+   
    private final TimerTask syncTask = new TimerTask() {
 
       @Override
       public void run() {
-         
 
       }
    };
    private final Timer syncTimer = new Timer();
 
-   private final Thread sync = new Thread(new Runnable() {
-      @Override
-      public void run() {
-         MulticastSocket socket;
-
-         try {
-
-            byte[] buffer = new byte[BUFFER_SIZE];
-            byte id;
-
-            socket = new MulticastSocket(BROADCAST_PORT);
-            InetAddress group = InetAddress.getByName(GROUP_ADDRESS);
-            socket.joinGroup(group);
-            
-            DatagramPacket paquet = new DatagramPacket(buffer, BUFFER_SIZE);
-
-            do {
-               // wait for Master
-               socket.receive(paquet);
-
-               if (paquet.getLength() == 2 && paquet.getData()[0] == 0) {
-                  id = paquet.getData()[1];
-                  masterAddress = paquet.getAddress();
-               }
-            } while (masterAddress == null);
-            
-            // Wait for FollowUp
-            socket.receive(paquet);
-            
-
-         } catch (IOException ex) {
-            Logger.getLogger(SlavePTP.class.getName()).log(Level.SEVERE, null, ex);
-         }
+   private final Thread sync = new Thread(() -> {
+      try {
+         waitSync();
+      } catch (IOException ex) {
+         Logger.getLogger(SlavePTP.class.getName()).log(Level.SEVERE, null, ex);
       }
    });
+
+   private void waitSync() throws IOException {
+      byte[] buffer = new byte[BUFFER_SIZE];
+      Byte id = null;
+
+      socket = new MulticastSocket(BROADCAST_PORT);
+      InetAddress group = InetAddress.getByName(GROUP_ADDRESS);
+      socket.joinGroup(group);
+
+      DatagramPacket paquet = new DatagramPacket(buffer, BUFFER_SIZE);
+
+      do {
+         // wait for Master
+         socket.receive(paquet);
+
+         if (paquet.getLength() == 2 && paquet.getData()[0] == 0) {
+            id = paquet.getData()[1];
+            masterAddress = paquet.getAddress();
+         }
+      } while (masterAddress == null);
+
+      // Wait for FollowUp
+      socket.receive(paquet);
+
+      if (paquet.getLength() == 3 && paquet.getData()[0] == 1 && paquet.getData()[1] == id) {
+
+      }
+   }
 
    private void startThread() {
       System.out.println("Thread start");
       sync.start();
       System.out.println("Thread started");
    }
-
-   public SlavePTP() throws SocketException, IOException {
-      //Diffusion des messages sync et followup
-      startThread();
-
-      //syncTimer.scheduleAtFixedRate(syncTask, 0, SYNC_PERIOD);
+   
+   public long getTimeSynced(){
+      return System.currentTimeMillis() + delay;
    }
 
    @Override
@@ -90,8 +95,4 @@ public class SlavePTP {
       sync.join();
    }
 
-   public static void main(String ... args) throws IOException{
-	   SlavePTP p = new SlavePTP();
-   }
-   
 }
