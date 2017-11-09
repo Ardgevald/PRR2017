4871c1ed89ddb26ed8b03ef8147b120bc6028e61
MiguelPomboDias <miguel.pombodias@heig-vd.ch>
2017-11-09 11:11:30 +0100

    init Labo2

---
+++ Labo02_RMI/GlobalVariableClient/src/main/java/ch/heigvd/globalvariableclient/App.java
@@ -0,0 +1,12 @@
+
+package ch.heigvd.globalvariableclient;
+
+/**
+ *
+ */
+public class App {
+   
+   public static void main(String... args) {
+      System.out.println("Test");
+   }
+}
---
+++ Labo02_RMI/LamportManager/src/main/java/ch/heigvd/lamportmanager/LamportManager.java
@@ -0,0 +1,11 @@
+package ch.heigvd.lamportmanager;
+
+/**
+ *
+ */
+public class LamportManager {
+
+   public static void main(String... args) {
+      System.out.println("Test");
+   }
+}
