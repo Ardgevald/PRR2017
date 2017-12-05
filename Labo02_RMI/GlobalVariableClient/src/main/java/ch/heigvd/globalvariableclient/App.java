9f664ea28b090edabbfeded5723b11ecc4a58866
MiguelPomboDias <miguel.pombodias@heig-vd.ch>
2017-12-05 18:58:30 +0100

    ready for tests

--- Labo02_RMI/GlobalVariableClient/src/main/java/ch/heigvd/globalvariableclient/App.java
+++ Labo02_RMI/GlobalVariableClient/src/main/java/ch/heigvd/globalvariableclient/App.java
@@ -13,40 +13,41 @@ import java.util.logging.Logger;
  */
 public class App {
 
-	private IGlobalVariable server;
+   private IGlobalVariable server;
 
-	public App(String site) {
-		// Rechercher une reference au serveur
-		try {
-			server = (IGlobalVariable) Naming.lookup("//" + site + "/GlobalVariable");
-		} catch (MalformedURLException | NotBoundException | RemoteException e) {
-			System.out.println("Erreur avec la reference du serveur");
-			e.printStackTrace();
-			System.exit(1);
-		}
-	}
+   public App(String site) {
+      // Rechercher une reference au serveur
+      try {
+         server = (IGlobalVariable) Naming.lookup("//" + site + "/GlobalVariable");
+      } catch (MalformedURLException | NotBoundException | RemoteException e) {
+         System.out.println("Erreur avec la reference du serveur");
+         e.printStackTrace();
+         System.exit(1);
+      }
+   }
 
-	public int getGlobalVariable() throws RemoteException {
-		return server.getVariable();
-	}
+   public int getGlobalVariable() throws RemoteException {
+      return server.getVariable();
+   }
 
-	public void setGlobalValue(int value) throws RemoteException {
-		server.setVariable(value);
-	}
+   public void setGlobalValue(int value) throws RemoteException {
+      server.setVariable(value);
+   }
 
-	public static void main(String... args) {
+   public static void main(String... args) {
 
-		// Creating 5 app per server
-		/*
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 5; j++) {
+      // Creating 5 app per server
+      //*
+		for (int i = 0; i < 1; i++) {
+			for (int j = 0; j < 2; j++) {
 				final App application = new App("localhost:" + (2002 + i));
-
+            final String name = i + " - " + j + " : ";
 				new Thread(() -> {
+               
 					for (int x = 0; x < 60; x++) {
 						try {
 							application.setGlobalValue(x);
-							System.out.println(application.getGlobalVariable());
+							System.out.println(name + application.getGlobalVariable());
 						} catch (RemoteException ex) {
 							Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
 						}
@@ -55,21 +56,20 @@ public class App {
 
 			}
 		}//*/
-		//*
-		try {
-			//System.setProperty("java.security.policy", "file:./ch/heigvd/globalvariableclient/client.policy");
-			//App application = new App(args[0]);
-			App application = new App("localhost:2002");
-			System.out.println(application.getGlobalVariable());
-			application.setGlobalValue(23);
-			System.out.println(application.getGlobalVariable());
-			
-		
-		} catch (IndexOutOfBoundsException e) {
-			System.out.println("Usage: " + App.class.getSimpleName() + " [site_serveur]");
-			System.exit(1);
-		} catch (RemoteException ex) {
-			Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
-		}//*/
-	}
+      /*
+      try {
+         //System.setProperty("java.security.policy", "file:./ch/heigvd/globalvariableclient/client.policy");
+         //App application = new App(args[0]);
+         App application = new App("localhost:2002");
+         System.out.println(application.getGlobalVariable());
+         application.setGlobalValue(23);
+         System.out.println(application.getGlobalVariable());
+
+      } catch (IndexOutOfBoundsException e) {
+         System.out.println("Usage: " + App.class.getSimpleName() + " [site_serveur]");
+         System.exit(1);
+      } catch (RemoteException ex) {
+         Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+      }//*/
+   }
 }
--- Labo02_RMI/LamportManager/src/main/java/ch/heigvd/lamportmanager/LamportManager.java
+++ Labo02_RMI/LamportManager/src/main/java/ch/heigvd/lamportmanager/LamportManager.java
@@ -21,269 +21,275 @@ import java.util.logging.Logger;
  */
 public class LamportManager {
 
-	private static final Logger LOG = Logger.getLogger(LamportManager.class.getName());
+   private static final Logger LOG = Logger.getLogger(LamportManager.class.getName());
 
-	private final static String VARIABLE_SERVER_NAME = "GlobalVariable";
-	private final static String LAMPORT_SERVER_NAME = "Lamport";
+   private final static String VARIABLE_SERVER_NAME = "GlobalVariable";
+   private final static String LAMPORT_SERVER_NAME = "Lamport";
 
-	private int globalVariable;
-	private final int nbSites;
-	private final int hostIndex;
+   private int globalVariable;
+   private final int nbSites;
+   private final int hostIndex;
 
-	private long localTimeStamp = 0;
+   private long localTimeStamp = 0;
 
-	private ILamportAlgorithm[] lamportServers;
+   private ILamportAlgorithm[] lamportServers;
 
-	String[][] remotes;
+   private final Object lock = new Object();
 
-	private static class Message {
+   String[][] remotes;
 
-		public static enum MESSAGE_TYPE {
-			REQUEST, RESPONSE, LIBERATE
-		};
-		private MESSAGE_TYPE messageType;
-		private long time;
+   private static class Message {
 
-		public Message(MESSAGE_TYPE message_type, long time) {
-			this.messageType = message_type;
-			this.time = time;
-		}
+      public static enum MESSAGE_TYPE {
+         REQUEST, RESPONSE, LIBERATE
+      };
+      private MESSAGE_TYPE messageType;
+      private long time;
 
-		public MESSAGE_TYPE getMessageType() {
-			return messageType;
-		}
+      public Message(MESSAGE_TYPE message_type, long time) {
+         this.messageType = message_type;
+         this.time = time;
+      }
 
-		public long getTime() {
-			return time;
-		}
+      public MESSAGE_TYPE getMessageType() {
+         return messageType;
+      }
 
-	}
+      public long getTime() {
+         return time;
+      }
 
-	private Message[] messagesArray;
+   }
 
-	public static void main(String... args) throws RemoteException, IOException {
-		// Creating hosts
-		LamportManager[] lamportManagers  = {
-			new LamportManager(0), new LamportManager(1), new LamportManager(2)
-		};
-		
-		// Connecting hosts
-		Arrays.stream(lamportManagers).forEach(LamportManager::connectToRemotes);
-	}
+   private Message[] messagesArray;
 
-	public LamportManager(int hostIndex) throws IOException {
-		this.hostIndex = hostIndex;
+   public static void main(String... args) throws RemoteException, IOException {
+      // Creating hosts
+      LamportManager[] lamportManagers = {
+         new LamportManager(0), new LamportManager(1), new LamportManager(2)
+      };
 
-		// TODO : supprimer ce set -----------------------------------asdfasdf §asdf 3qfqwe
-		globalVariable = 57;
+      // Connecting hosts
+      Arrays.stream(lamportManagers).forEach(LamportManager::connectToRemotes);
+   }
 
-		// Retreiving the other hosts
-		remotes = Files.readAllLines(Paths.get("hosts.txt")).stream()
-				.map((s) -> s.split(" "))
-				.toArray(String[][]::new);
-		this.nbSites = remotes.length;
+   public LamportManager(int hostIndex) throws IOException {
+      this.hostIndex = hostIndex;
 
-		this.lamportServers = new ILamportAlgorithm[nbSites];
-		
-		// On initialise les messages reçu à un temps 0 avec des liberates
-		this.messagesArray = new Message[nbSites];
-		for(int i = 0; i < messagesArray.length; i++){
-			this.messagesArray[i] = new Message(Message.MESSAGE_TYPE.LIBERATE, 0);
-		}
+      // TODO : supprimer ce set -----------------------------------asdfasdf §asdf 3qfqwe
+      globalVariable = 57;
 
-		try {
+      // Retreiving the other hosts
+      remotes = Files.readAllLines(Paths.get("hosts.txt")).stream()
+              .map((s) -> s.split(" "))
+              .toArray(String[][]::new);
+      this.nbSites = remotes.length;
 
-			// Retreiving our port address
-			int portUsed = Integer.parseInt(remotes[hostIndex][1]);
+      this.lamportServers = new ILamportAlgorithm[nbSites];
 
-			// Creating local server
-			IGlobalVariable globalVariableServer = new GlobalVariableServer();
-			// On lie dans le registry
-			Registry registry = LocateRegistry.createRegistry(portUsed);
-			registry.rebind(VARIABLE_SERVER_NAME, globalVariableServer);
+      // On initialise les messages reçu à un temps 0 avec des liberates
+      this.messagesArray = new Message[nbSites];
+      for (int i = 0; i < messagesArray.length; i++) {
+         this.messagesArray[i] = new Message(Message.MESSAGE_TYPE.LIBERATE, 0);
+      }
 
-			ILamportAlgorithm lamportAlgorithmServer = new LamportAlgorithmServer();
-			registry.rebind(LAMPORT_SERVER_NAME, lamportAlgorithmServer);
+      try {
 
-			System.out.println("RMI registry on " + portUsed + " with bindings:");
-			Arrays.stream(registry.list()).forEach(System.out::println);
+         // Retreiving our port address
+         int portUsed = Integer.parseInt(remotes[hostIndex][1]);
 
-		} catch (RemoteException ex) {
-			Logger.getLogger(LamportManager.class.getName()).log(Level.SEVERE, null, ex);
-		}
-	}
+         // Creating local server
+         IGlobalVariable globalVariableServer = new GlobalVariableServer();
+         // On lie dans le registry
+         Registry registry = LocateRegistry.createRegistry(portUsed);
+         registry.rebind(VARIABLE_SERVER_NAME, globalVariableServer);
 
-	public void connectToRemotes() {
-		// Connecting to other hosts
-		for (int i = 0; i < remotes.length; i++) {
-			String[] currentHost = remotes[i];
-			try {
-				ILamportAlgorithm remoteServer = (ILamportAlgorithm) Naming.lookup("//" + currentHost[0] + ":" + currentHost[1] + "/" + LAMPORT_SERVER_NAME);
-				lamportServers[i] = remoteServer;
-			} catch (NotBoundException | MalformedURLException | RemoteException ex) {
-				Logger.getLogger(LamportManager.class.getName()).log(Level.SEVERE, null, ex);
-			}
-		}
-		
-		System.out.println("Remotes connected !");
-	}
+         ILamportAlgorithm lamportAlgorithmServer = new LamportAlgorithmServer();
+         registry.rebind(LAMPORT_SERVER_NAME, lamportAlgorithmServer);
 
-	private synchronized void sendRequestsAndProcessResponse(final long localTimeStamp) throws InterruptedException {
-		// On set notre message courant
-		messagesArray[hostIndex] = new Message(Message.MESSAGE_TYPE.REQUEST, localTimeStamp);
+         System.out.println("RMI registry on " + portUsed + " with bindings:");
+         Arrays.stream(registry.list()).forEach(System.out::println);
 
-		Thread[] senderThreads = new Thread[lamportServers.length];
+      } catch (RemoteException ex) {
+         Logger.getLogger(LamportManager.class.getName()).log(Level.SEVERE, null, ex);
+      }
+   }
 
-		// Création des threads d'envoi des requêtes
-		for (int i = 0; i < senderThreads.length; i++) {
-			final int index = i;
-			senderThreads[i] = new Thread(() -> {
-				try {
-					// On envoie à tout le monde
-					/**
-					 * NOTE IMPORTANTE : on envoie aussi à soit même, ce qui a
-					 * pour effet d'y ajouter notre requête dans la liste local
-					 */
-					long remoteTime = lamportServers[index].request(localTimeStamp, hostIndex);
+   public void connectToRemotes() {
+      // Connecting to other hosts
+      for (int i = 0; i < remotes.length; i++) {
+         String[] currentHost = remotes[i];
+         try {
+            ILamportAlgorithm remoteServer = (ILamportAlgorithm) Naming.lookup("//" + currentHost[0] + ":" + currentHost[1] + "/" + LAMPORT_SERVER_NAME);
+            lamportServers[i] = remoteServer;
+         } catch (NotBoundException | MalformedURLException | RemoteException ex) {
+            Logger.getLogger(LamportManager.class.getName()).log(Level.SEVERE, null, ex);
+         }
+      }
 
-					// On set le message reçu
-					handleMessageReceived(index, new Message(Message.MESSAGE_TYPE.RESPONSE, remoteTime));
-				} catch (RemoteException ex) {
-					Logger.getLogger(LamportManager.class.getName()).log(Level.SEVERE, null, ex);
-				}
-			});
-		}
+      System.out.println("Remotes connected !");
+   }
 
-		// Exécution des threads
-		for (Thread senderThread : senderThreads) {
-			senderThread.start();
-		}
+   private synchronized void sendRequestsAndProcessResponse(final long localTimeStamp) throws InterruptedException {
+      // On set notre message courant
+      messagesArray[hostIndex] = new Message(Message.MESSAGE_TYPE.REQUEST, localTimeStamp);
 
-		// Attente sur les threads (bloquant)
-		for (Thread senderThread : senderThreads) {
-			senderThread.join();
-		}
+      Thread[] senderThreads = new Thread[lamportServers.length];
 
-	}
+      // Création des threads d'envoi des requêtes
+      for (int i = 0; i < senderThreads.length; i++) {
+         final int index = i;
+         senderThreads[i] = new Thread(() -> {
+            try {
+               // On envoie à tout le monde
+               /**
+                * NOTE IMPORTANTE : on envoie aussi à soit même, ce qui a pour effet
+                * d'y ajouter notre requête dans la liste local
+                */
+               long remoteTime = lamportServers[index].request(localTimeStamp, hostIndex);
 
-	private synchronized void sendLiberates(long localTimeStamp, int value) throws RemoteException {
-		// la méthode free n'est pas bloquante
-		for (ILamportAlgorithm lamportServer : lamportServers) {
-			lamportServer.free(localTimeStamp, value, hostIndex);
-		}
-	}
+               // On set le message reçu
+               handleMessageReceived(index, new Message(Message.MESSAGE_TYPE.RESPONSE, remoteTime));
+            } catch (RemoteException ex) {
+               Logger.getLogger(LamportManager.class.getName()).log(Level.SEVERE, null, ex);
+            }
+         });
+      }
 
-	private void increaseTime(long remoteTimeStamp) {
-		localTimeStamp = Math.max(localTimeStamp, remoteTimeStamp) + 1;
-	}
+      // Exécution des threads
+      for (Thread senderThread : senderThreads) {
+         senderThread.start();
+      }
 
-	private class GlobalVariableServer extends UnicastRemoteObject implements IGlobalVariable {
+      // Attente sur les threads (bloquant)
+      for (Thread senderThread : senderThreads) {
+         senderThread.join();
+      }
 
-		public GlobalVariableServer() throws RemoteException {
-			super();
-		}
+   }
 
-		@Override
-		public int getVariable() throws RemoteException {
-			return globalVariable;
-		}
+   private synchronized void sendLiberates(long localTimeStamp, int value) throws RemoteException {
+      // la méthode free n'est pas bloquante
+      for (ILamportAlgorithm lamportServer : lamportServers) {
+         lamportServer.free(localTimeStamp, value, hostIndex);
+      }
+   }
 
-		@Override
-		public void setVariable(int value) throws RemoteException {
-			// Demande de section critique
-			waitForCS();
-			// On est ici en section critique
+   private void increaseTime(long remoteTimeStamp) {
+      localTimeStamp = Math.max(localTimeStamp, remoteTimeStamp) + 1;
+   }
 
-			globalVariable = value;
+   private class GlobalVariableServer extends UnicastRemoteObject implements IGlobalVariable {
 
-			// Relachement de la section critique				
-			releaseCS();
+      public GlobalVariableServer() throws RemoteException {
+         super();
+      }
 
-		}
+      @Override
+      public int getVariable() throws RemoteException {
+         return globalVariable;
+      }
 
-	}
+      @Override
+      public void setVariable(int value) throws RemoteException {
+         // Demande de section critique
+         waitForCS();
+         // On est ici en section critique
 
-	private void handleMessageReceived(int hostIndex, Message message) {
-		// On replace pas les messages de type requêtes par une quittance
-		if (message.messageType != Message.MESSAGE_TYPE.RESPONSE || messagesArray[hostIndex].messageType != Message.MESSAGE_TYPE.REQUEST) {
-			messagesArray[hostIndex] = message;
-		}
+         globalVariable = value;
 
-	}
+         // Relachement de la section critique				
+         releaseCS();
 
-	private class LamportAlgorithmServer extends UnicastRemoteObject implements ILamportAlgorithm {
+      }
 
-		public LamportAlgorithmServer() throws RemoteException {
-			super();
-		}
+   }
 
-		@Override
-		public synchronized long request(long remoteTimeStamp, int hostIndex) throws RemoteException {
-			increaseTime(remoteTimeStamp);
+   private void handleMessageReceived(int hostIndex, Message message) {
+      // On replace pas les messages de type requêtes par une quittance
+      if (message.messageType != Message.MESSAGE_TYPE.RESPONSE || messagesArray[hostIndex].messageType != Message.MESSAGE_TYPE.REQUEST) {
+         messagesArray[hostIndex] = message;
+      }
 
-			handleMessageReceived(hostIndex, new Message(Message.MESSAGE_TYPE.REQUEST, remoteTimeStamp));
+   }
 
-			// On quittance en envoyant le temps courant
-			return localTimeStamp;
-		}
+   private class LamportAlgorithmServer extends UnicastRemoteObject implements ILamportAlgorithm {
 
-		@Override
-		public synchronized void free(long remoteTimeStamp, int value, int hostIndex) throws RemoteException {
-			globalVariable = value;
+      public LamportAlgorithmServer() throws RemoteException {
+         super();
+      }
 
-			// On met à jour le temps local
-			increaseTime(remoteTimeStamp);
+      @Override
+      public synchronized long request(long remoteTimeStamp, int hostIndex) throws RemoteException {
+         increaseTime(remoteTimeStamp);
 
-			// On met à jour les messages reçu
-			handleMessageReceived(hostIndex, new Message(Message.MESSAGE_TYPE.LIBERATE, remoteTimeStamp));
+         handleMessageReceived(hostIndex, new Message(Message.MESSAGE_TYPE.REQUEST, remoteTimeStamp));
 
-			// On notifie si on souhaitait, par hasard, entrer en section critique
-			LamportManager.this.notify();
-		}
+         // On quittance en envoyant le temps courant
+         return localTimeStamp;
+      }
 
-	}
+      @Override
+      public void free(long remoteTimeStamp, int value, int hostIndex) throws RemoteException {
+         globalVariable = value;
 
-	private synchronized void waitForCS() {
-		try {
-			sendRequestsAndProcessResponse(localTimeStamp);
+         // On met à jour le temps local
+         increaseTime(remoteTimeStamp);
 
-			/*
-			* On calcule si on peut entrer en SC
-			* On reste bloqué tant qu'on peut pas entrer en SC
-			 */
-			while (!canEnterCS()) {
-				// Attendre jusqu'à ce qu'on soit notifié lors de l'arrivée d'un message
-				this.wait();
-			}
+         // On met à jour les messages reçu
+         handleMessageReceived(hostIndex, new Message(Message.MESSAGE_TYPE.LIBERATE, remoteTimeStamp));
 
-		} catch (InterruptedException ex) {
-			Logger.getLogger(LamportManager.class.getName()).log(Level.SEVERE, null, ex);
-		}
-	}
+         // On notifie si on souhaitait, par hasard, entrer en section critique
+         synchronized (lock) {
+            lock.notify();
+         }
+      }
 
-	private synchronized void releaseCS() {
-		try {
-			sendLiberates(localTimeStamp, globalVariable);
-		} catch (RemoteException ex) {
-			Logger.getLogger(LamportManager.class.getName()).log(Level.SEVERE, null, ex);
-		}
-	}
+   }
 
-	private boolean canEnterCS() {
-		long minTime = Long.MAX_VALUE;
-		for (Message message : messagesArray) {
-			if (message.time < minTime) {
-				minTime = message.time;
-			}
-		}
+   private void waitForCS() {
+      try {
+         sendRequestsAndProcessResponse(localTimeStamp);
 
-		/**
-		 * "Un processus Pi se donne le droit d'entrer en section critique
-		 * lorsque file(i).msgType = REQUETE et que son estampille est la plus
-		 * ancienne des messages contenus dans file(i)."
-		 */
-		return messagesArray[hostIndex].messageType == Message.MESSAGE_TYPE.REQUEST
-				&& messagesArray[hostIndex].time == minTime;
-	}
+         /*
+         * On calcule si on peut entrer en SC
+         * On reste bloqué tant qu'on peut pas entrer en SC
+          */
+         while (!canEnterCS()) {
+            synchronized (lock) {
+               // Attendre jusqu'à ce qu'on soit notifié lors de l'arrivée d'un message
+               lock.wait();
+            }
+         }
+
+      } catch (InterruptedException ex) {
+         Logger.getLogger(LamportManager.class.getName()).log(Level.SEVERE, null, ex);
+      }
+   }
+
+   private synchronized void releaseCS() {
+      try {
+         sendLiberates(localTimeStamp, globalVariable);
+      } catch (RemoteException ex) {
+         Logger.getLogger(LamportManager.class.getName()).log(Level.SEVERE, null, ex);
+      }
+   }
+
+   private boolean canEnterCS() {
+      long minTime = Long.MAX_VALUE;
+      for (Message message : messagesArray) {
+         if (message.time < minTime) {
+            minTime = message.time;
+         }
+      }
+
+      /**
+       * "Un processus Pi se donne le droit d'entrer en section critique lorsque
+       * file(i).msgType = REQUETE et que son estampille est la plus ancienne des
+       * messages contenus dans file(i)."
+       */
+      return messagesArray[hostIndex].messageType == Message.MESSAGE_TYPE.REQUEST
+              && messagesArray[hostIndex].time == minTime;
+   }
 
 }
