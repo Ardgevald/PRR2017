e2b3aed51350665dd8d5e7be61359e319745f426
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2018-01-04 18:48:00 +0100

    startElection should work

--- Labo03_Election/src/main/java/ch/heigvd/prr/election/ElectionManager.java
+++ Labo03_Election/src/main/java/ch/heigvd/prr/election/ElectionManager.java
@@ -5,6 +5,7 @@
  */
 package ch.heigvd.prr.election;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -17,29 +18,43 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.LinkedList;
+import util.ByteIntConverter;
 
 /**
  *
  * @author Remi
  */
-public class ElectionManager {
+public class ElectionManager implements Closeable {
 
 	private LinkedList<Site> ring;
 	private Site[] hosts;
 	private final Site localSite;
+	private final int localHostIndex;
+	private Site neighbor;
 	private Site elu = null;
+	
 
 	private DatagramSocket socket;
 	
+	/**
+	 * Un message de type annonce est formé de d'abord le type de message, puis
+	 * des aptitudes (int) de chacun :
+	 * |TYPE|APT1|APT1|APT1|APT1|APT2|APT2|APT2|APT2|...
+	 */
+	private int getAnnounceSize(){
+		return 1 + Integer.BYTES * hosts.length;
+	}
+
 	private boolean isAnnouncing = false;
-	private static enum MessageType{
+
+	private static enum MessageType {
 		ANNOUNCE, RESULTS;
-		
-		public byte getByte(){
-			return (byte)this.ordinal();
+
+		public byte getByte() {
+			return (byte) this.ordinal();
 		}
-		
-		public static MessageType getMessageType(byte b){
+
+		public static MessageType getMessageType(byte b) {
 			return MessageType.values()[b];
 		}
 	}
@@ -47,37 +62,46 @@ public class ElectionManager {
 	private Thread electionListener = new Thread(() -> {
 		while (true) {
 			try {
-				DatagramPacket packet = new DatagramPacket(new byte[4], 4);
-				
+				DatagramPacket packet = new DatagramPacket(new byte[getAnnounceSize()], getAnnounceSize());
+
 				// We are waiting for an income packet
 				socket.receive(packet);
-				
+
 				MessageType messageType = MessageType.getMessageType(packet.getData()[0]);
-				
-				switch(messageType){
+
+				switch (messageType) {
 					case ANNOUNCE:
 						break;
 					case RESULTS:
 						break;
 				}
-								
-				
+
 			} catch (IOException e) {
 
 			}
 		}
 	});
 
-	private int evaluateAptitude(){
+	private int evaluateAptitude() {
 		return socket.getLocalAddress().getAddress()[3] + socket.getLocalPort();
 	}
-	
+
+	/**
+	 * Main constructor
+	 * @param hosts
+	 * @param hostIndex
+	 * @throws SocketException 
+	 */
 	public ElectionManager(Site[] hosts, int hostIndex) throws SocketException {
 		this.hosts = hosts;
 		this.localSite = hosts[hostIndex];
+		this.localHostIndex = hostIndex;
+
+		// Getting the neighbor
+		neighbor = hosts[hostIndex + 1 % hosts.length];
 		
 		// Creating the main socket
-		socket = new DatagramSocket(new InetSocketAddress(localSite.ip, localSite.port));
+		socket = new DatagramSocket(localSite.socketAddress);
 
 		// Launching the listening thread
 		electionListener.start();
@@ -97,19 +121,41 @@ public class ElectionManager {
 			.toArray(String[][]::new), hostIndex);
 	}
 
-	public void startElection() {
+	public void startElection() throws IOException {		
+		//-- Preparing the message 
+		byte[] message = new byte[getAnnounceSize()];		
+		
+		message[0] = MessageType.ANNOUNCE.getByte();
+		addAptitudeToMessage(message);
+		
+		// Sending it
+		DatagramPacket packet = new DatagramPacket(message, message.length, neighbor.socketAddress);
+		socket.send(packet);
+		
+		isAnnouncing = true;
+	}
 
+	private void addAptitudeToMessage(byte[] message) {
+		// Setting aptitude
+		byte[] aptitude =  ByteIntConverter.intToByte(evaluateAptitude());
+		for(int i = 0; i < aptitude.length; i++){
+			message[1 + localHostIndex * (aptitude.length)] = aptitude[i];
+		}
+	}
+
+	@Override
+	public void close() throws IOException {
+		socket.close();
+		// TODO arrêter la boucle
 	}
 
 	public static class Site {
 
 		private String ip;
-		private int port;
-		private int aptitude;
+		private SocketAddress socketAddress;
 
 		public Site(String ip, int port) {
-			this.ip = ip;
-			this.port = port;
+			socketAddress = new InetSocketAddress(ip, port);
 		}
 
 	}
--- Labo03_Election/src/main/java/ch/heigvd/prr/election/util/ByteIntConverter.java
+++ Labo03_Election/src/main/java/ch/heigvd/prr/election/util/ByteIntConverter.java
@@ -3,30 +3,30 @@ package util;
 import java.nio.ByteBuffer;
 
 /**
- * Classe utilitaire pour la conversion entre long et byte
+ * Classe utilitaire pour la conversion entre int et byte
  */
-public class ByteLongConverter {
+public class ByteIntConverter {
 
 	/**
-	 * Convertit un long en tableau de bytes
-	 * @param x le long à convertir
+	 * Convertit un int en tableau de bytes
+	 * @param x le int à convertir
 	 * @return le tableau de bytes correspondant
 	 */
-	public static byte[] longToBytes(long x) {
-		ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
-		buffer.putLong(x);
+	public static byte[] intToByte(int x) {
+		ByteBuffer buffer = ByteBuffer.allocate(Integer.BYTES);
+		buffer.putInt(x);
 		return buffer.array();
 	}
 
 	/**
-	 * Convertit un tableau de bytes en long
+	 * Convertit un tableau de bytes en int
 	 * @param bytes le tableau à convertir
-	 * @return le long correspondant
+	 * @return le int correspondant
 	 */
-	public static long bytesToLong(byte[] bytes) {
-		ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+	public static int bytesToInt(byte[] bytes) {
+		ByteBuffer buffer = ByteBuffer.allocate(Integer.BYTES);
 		buffer.put(bytes);
 		buffer.flip();//need flip 
-		return buffer.getLong();
+		return buffer.getInt();
 	}
 }
